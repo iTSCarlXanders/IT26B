e9d3df9ff17bbdab0ecd172d0991e3191124a65e
iTSCarlXander <xanderdayrit6@gmail.com>
2026-04-12 21:16:54 +0800

    add place holders sa login unya gipa chuyan wahahah

--- SIMPLE_GUI/src/GUI/Register.java
+++ SIMPLE_GUI/src/GUI/Register.java
@@ -126,6 +126,7 @@ public class Register extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jPasswordField1 = new javax.swing.JPasswordField();
         jPasswordField2 = new javax.swing.JPasswordField();
+        jButton2 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jPanel3 = new javax.swing.JPanel();
@@ -197,7 +198,7 @@ public class Register extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
-        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 420, 198, -1));
+        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(138, 420, -1, -1));
 
         jLabel5.setFont(new java.awt.Font("Segoe Print", 1, 24)); // NOI18N
         jLabel5.setText("Shinobi Enrollment");
@@ -213,6 +214,17 @@ public class Register extends javax.swing.JFrame {
 
         jPasswordField2.setText("jPasswordField2");
         jPanel2.add(jPasswordField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 370, 200, 30));
+
+        jButton2.setBackground(new java.awt.Color(153, 0, 0));
+        jButton2.setFont(new java.awt.Font("Segoe Print", 1, 15)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(255, 255, 255));
+        jButton2.setText("BACK");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 420, 80, 40));
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/scroll s (8) (5).png"))); // NOI18N
         jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(-100, 0, 420, 510));
@@ -288,6 +300,10 @@ public class Register extends javax.swing.JFrame {
        togglePassword(jPasswordField1, jCheckBox1, "Forbidden Seal: (Password)");
     }//GEN-LAST:event_jCheckBox1ActionPerformed
 
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -308,6 +324,7 @@ public class Register extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JCheckBox jCheckBox2;
     private javax.swing.JLabel jLabel1;
